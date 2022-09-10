d1a5415337fa0f1a6b4a6bd741617162f578a4e5
Yuliia Panchuk <55352643+YuliiaPanchuk@users.noreply.github.com>
2022-09-10 22:56:03 +0200

    Update ShoppingCart component

--- src/components/ShoppingCart/ShoppingCart.tsx
+++ src/components/ShoppingCart/ShoppingCart.tsx
@@ -1,7 +1,50 @@
+import React, { useEffect, useState } from 'react';
+import { getProductsById, ProductInfo } from '../../api/product';
+import { useShoppingCart } from '../../context/ShoppingCartContext';
+import { NavBar } from '../NavBar/NavBar';
+
 export function ShoppingCart() {
+  const { cartItems } = useShoppingCart();
+  const [product, setProduct] = useState<ProductInfo[]>([]);
+
+  useEffect(() => {
+    const product_ids = cartItems.map((item) => item.id);
+
+    getProductsById(product_ids).then((item) => {
+      setProduct(item as ProductInfo[]);
+    });
+  }, [cartItems]);
+
   return (
     <>
-      <h1>ShoppingCart</h1>
+      <NavBar />
+      <div className="shoppingCart">
+        <div className="shoppingCartContainer">
+          <div className="shoppingCartHeader">
+            <p className="shoppingCartTitle">My shopping cart</p>
+          </div>
+
+          <div className="shoppingCartWhiteBlock">
+            <p>Your items</p>
+
+            <ul>
+              <li>Products</li>
+              <li>Qty</li>
+              <li>Price</li>
+            </ul>
+
+            <>
+              {product.map((item) => (
+                <div key={item.product_id} className="shoppingCartProductWrapper">
+                  <img src={item.image_url} alt="something" />
+                  <p>{item.product_name}</p>
+                </div>
+              ))}
+            </>
+          </div>
+        </div>
+        <div></div>
+      </div>
     </>
-  )
+  );
 }

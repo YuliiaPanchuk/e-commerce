2ab1946a3125832c1fbf25577bb98a63039becd7
Yuliia Panchuk <55352643+YuliiaPanchuk@users.noreply.github.com>
2022-09-10 20:57:06 +0200

    Display number of products

--- src/components/NavBarIcons/NavBarIcons.tsx
+++ src/components/NavBarIcons/NavBarIcons.tsx
@@ -1,22 +1,36 @@
-import { useState } from 'react';
+import React from "react"
 import { ShoppingIcon } from './ShoppingIcon';
 import { UserProfile } from './UserProfile';
 import "./icons.css"
+import { useShoppingCart } from '../../context/ShoppingCartContext';
 
 export function NavBarIcons() {
-  const [showEmptyCart, setShowEmptyCart] = useState(false);
-
-  function handleShoppingIcon() {
-    setShowEmptyCart(true);
-  }
+  const { openCart, cartQuantity } = useShoppingCart();
 
   return (
     <div className="iconsNavBarWrapper">
       <ul className="iconNavBarUl">
         <li>
-          <button className="navbarIconBtn" onClick={handleShoppingIcon}>
-            <ShoppingIcon />
-          </button>
+            <button
+              onClick={openCart}
+              style={{ width: '3rem', height: '2rem', position: 'relative' }}
+              className="navbarIconBtn"
+            >
+              <ShoppingIcon />
+              <div
+                style={{
+                  color: 'black',
+                  width: '1rem',
+                  height: '1rem',
+                  position: 'absolute',
+                  bottom: 0,
+                  right: 0,
+                  transform: 'translate(-15%, -60%)'
+                }}>
+                {cartQuantity}
+              </div>
+            </button>
+         
         </li>
         <li>
           <button className="navbarIconBtn">

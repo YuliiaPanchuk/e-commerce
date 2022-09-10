5183875ff81ea0d62af8eec894e7d315662aad11
Yuliia Panchuk <55352643+YuliiaPanchuk@users.noreply.github.com>
2022-09-10 20:53:08 +0200

    Create Home component

---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import { NavBar } from "../components/NavBar/NavBar";
+import { UppBtn } from "../components/ScrollButton/UppBtn";
+
+export function Home() {
+  return (
+    <>
+      <NavBar />
+      <UppBtn />
+    </>
+  )
+}

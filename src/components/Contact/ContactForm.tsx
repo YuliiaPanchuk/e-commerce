1043d3a12468eebad8c868a29cc58cfb089dd1e8
Yuliia Panchuk <55352643+YuliiaPanchuk@users.noreply.github.com>
2022-09-17 21:52:53 +0200

    Create ContactForm css file

---
+++ src/components/Contact/ContactForm.tsx
@@ -0,0 +1,15 @@
+import './ContactForm.css';
+
+export function ContactForm() {
+  return (
+    <form method="POST" name="contactform" className="contactForm">
+      <input type="text" name="name" placeholder="Enter your name" />
+
+      <input type="email" name="email" placeholder="Enter your email" />
+
+      <textarea name="message" placeholder="Messaage"></textarea>
+
+      <button type="submit">Submit</button>
+    </form>
+  );
+}

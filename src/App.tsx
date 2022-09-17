33ff575e9c078baff51662b1ed452ddd673046f3
Yuliia Panchuk <55352643+YuliiaPanchuk@users.noreply.github.com>
2022-09-17 21:54:50 +0200

    Update App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { LogInForm } from './components/LogInForm/LogInForm';
 import { ShoppingCart } from './components/ShoppingCart/ShoppingCart';
 import { ShoppingCartProvider } from './context/ShoppingCartContext';
 import { Home } from './pages/Home';
+import { ItemDetails } from './pages/ItemDetails';
 import { Store } from './pages/Store';
 
 function App() {
@@ -14,6 +16,8 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/store" element={<Store />} />
             <Route path="/shoppingCart" element={<ShoppingCart />} />
+            <Route path="/itemDetails" element={<ItemDetails />} />
+            <Route path='/logIn' element={<LogInForm /> } />
           </Routes>
         </BrowserRouter>
       </div>

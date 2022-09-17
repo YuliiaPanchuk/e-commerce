803ca9093ff5420351713a966427678422922f18
Yuliia Panchuk <55352643+YuliiaPanchuk@users.noreply.github.com>
2022-09-17 21:56:04 +0200

    Update ShoppingCart component

--- src/components/ShoppingCart/ShoppingCart.tsx
+++ src/components/ShoppingCart/ShoppingCart.tsx
@@ -2,10 +2,13 @@ import React, { useEffect, useState } from 'react';
 import { getProductsById, ProductInfo } from '../../api/product';
 import { useShoppingCart } from '../../context/ShoppingCartContext';
 import { NavBar } from '../NavBar/NavBar';
-import "./ShoppingCart.css"
+import { QtyBtn } from '../QtyBtn/QtyBtn';
+import { RemoveBtn } from '../RemoveBtn/RemoveBtn';
+import './ShoppingCart.css';
 
 export function ShoppingCart() {
-  const { cartItems } = useShoppingCart();
+  const { cartItems, removeFromCart, decreaseCartQuantity, increaseCartQuantity, getItemQuantity } =
+    useShoppingCart();
   const [product, setProduct] = useState<ProductInfo[]>([]);
 
   useEffect(() => {
@@ -17,37 +20,56 @@ export function ShoppingCart() {
   }, [cartItems]);
 
   return (
-    <>
-      <NavBar />
-      <div className="shoppingCart">
-        <div className="shoppingCartContainer">
-
-          <div className="shoppingCartHeader">
-            <p className="shoppingCartTitle">My shopping cart</p>
-          </div>
-
-          <div className="shoppingCartWhiteBlock">
-            <p>Your items</p>
-
-            <ul className="shoppingCartUl">
-              <li>Products</li>
-              <li>Qty</li>
-              <li>Price</li>
-            </ul>
-
-            <>
-              {product.map((item) => (
-                <div key={item.product_id} className="shoppingCartProductWrapper">
-                  <div className="shoppingCartImage">
-                    <img src={item.image_url} alt="Our product" />
-                  </div>
-                  <p>{item.product_name}</p>
+    <div>
+      {product.map((item) => {
+        const quantity = getItemQuantity(item.product_id);
+        return (
+          <>
+            <NavBar />
+            <div className="shoppingCart">
+              <div className="shoppingCartContainer">
+                <div className="shoppingCartHeader">
+                  <p className="shoppingCartTitle">My shopping cart</p>
                 </div>
-              ))}
-            </>
-          </div>
-        </div>
-      </div>
-    </>
+
+                <div className="shoppingCartWhiteBlock">
+                  <p>Your items</p>
+
+                  <ul className="shoppingCartUl">
+                    <li>Products</li>
+                    <li>Qty</li>
+                    <li>Price</li>
+                  </ul>
+
+                  <div key={item.product_id} className="shoppingCartProductWrapper">
+                    <div className="shoppingCartImage">
+                      <img src={item.image_url} alt="Our product" />
+                    </div>
+
+                    <div className="productsSection">
+                      <p>{item.product_name}</p>
+                      <p>{item.product_description}</p>
+                      <RemoveBtn onClick={() => removeFromCart(item.product_id)} />
+                    </div>
+
+                    <div className="qtyButton">
+                      <QtyBtn
+                        decr={() => decreaseCartQuantity(item.product_id)}
+                        incr={() => increaseCartQuantity(item.product_id)}
+                        quantity={quantity}
+                      />
+                    </div>
+
+                    <div className="priceSection">
+                      <p>{ item.product_price}</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </>
+        );
+      })}
+    </div>
   );
 }

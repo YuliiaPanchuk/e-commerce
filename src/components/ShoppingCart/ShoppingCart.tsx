32147cbd91c78e317b3e0230d4706fe7c8d6b935
Yuliia Panchuk <55352643+YuliiaPanchuk@users.noreply.github.com>
2022-09-25 07:38:18 +0200

    Update ShoppingCart

--- src/components/ShoppingCart/ShoppingCart.tsx
+++ src/components/ShoppingCart/ShoppingCart.tsx
@@ -2,74 +2,121 @@ import React, { useEffect, useState } from 'react';
 import { getProductsById, ProductInfo } from '../../api/product';
 import { useShoppingCart } from '../../context/ShoppingCartContext';
 import { formatCurrency } from '../../utilities/formatCurrency';
-import { NavBar } from '../NavBar/NavBar';
 import { QtyBtn } from '../QtyBtn/QtyBtn';
 import { RemoveBtn } from '../RemoveBtn/RemoveBtn';
+import { DoneIcon } from './DoneIcon';
+import { LockIcon } from './LockIcon';
 import './ShoppingCart.css';
 
-export function ShoppingCart() {
+interface ShoppingCartProps {
+  products: ProductInfo[];
+}
+
+export function ShoppingCart({ products }: ShoppingCartProps) {
   const { cartItems, removeFromCart, decreaseCartQuantity, increaseCartQuantity, getItemQuantity } =
     useShoppingCart();
-  const [product, setProduct] = useState<ProductInfo[]>([]);
-
-  useEffect(() => {
-    const product_ids = cartItems.map((item) => item.id);
-
-    getProductsById(product_ids).then((item) => {
-      setProduct(item as ProductInfo[]);
-    });
-  }, [cartItems]);
 
   return (
     <div>
-      {product.map((item) => {
-        const quantity = getItemQuantity(item.product_id);
-        return (
-          <>
-            <NavBar />
-            <div className="shoppingCart">
-              <div className="shoppingCartContainer">
-                <div className="shoppingCartHeader">
-                  <p className="shoppingCartTitle">My shopping cart</p>
-                </div>
+      <div className="shoppingCart">
+        <div className="shoppingCartContainer">
+          
+          <div className="shoppingCartHeader">
+            <p className="shoppingCartTitle">My shopping cart</p>
+          </div>
 
-                <div className="shoppingCartWhiteBlock">
-                  <p>Your items</p>
+          <div className="shoppingCartWhiteBlock">
+            <div className="shoppingCartWhiteBlockContent">
+              <p>Your items</p>
 
-                  <ul className="shoppingCartUl">
-                    <li>Products</li>
-                    <li>Qty</li>
-                    <li>Price</li>
-                  </ul>
+              <ul className="shoppingCartUl">
+                <li>Products</li>
+                <li>Qty</li>
+                <li>Price</li>
+              </ul>
 
-                  <div key={item.product_id} className="shoppingCartProductWrapper">
-                    <div className="shoppingCartImage">
-                      <img src={item.image_url} alt="Our product" />
-                    </div>
+              {products.map((item) => {
+                const quantity = getItemQuantity(item.product_id);
+                return (
+                  <div>
+                    <div key={item.product_id} className="shoppingCartProductWrapper">
+                      <div className="shoppingCartImage">
+                        <img src={item.image_url} alt="Our product" />
+                      </div>
 
-                    <div className="productsSection">
-                      <p>{item.product_name}</p>
-                      <RemoveBtn onClick={() => removeFromCart(item.product_id)} />
-                    </div>
+                      <div className="productsSection">
+                        <div className="cartProductTittle">
+                          <p>{item.product_name}</p>
+                        </div>
+                        <div className="shoppingRemoveButton">
+                          <RemoveBtn onClick={() => removeFromCart(item.product_id)} />
+                        </div>
+                      </div>
 
-                    <div className="qtyButton">
-                      <QtyBtn
-                        decr={() => decreaseCartQuantity(item.product_id)}
-                        incr={() => increaseCartQuantity(item.product_id)}
-                        quantity={quantity}
-                      />
-                    </div>
+                      <div className="cartProductQtyButton">
+                        <QtyBtn
+                          decr={() => decreaseCartQuantity(item.product_id)}
+                          incr={() => increaseCartQuantity(item.product_id)}
+                          quantity={quantity}
+                        />
+                      </div>
 
-                    <div className="priceSection">
-                      <p>{ formatCurrency(item.product_price)}</p>
+                      <div className="priceSection">
+                        <p>
+                          {formatCurrency(item.product_price * getItemQuantity(item.product_id))}
+                        </p>
+                      </div>
                     </div>
                   </div>
-                </div>
-              </div>
+                );
+              })}
             </div>
-          </>
-        );
-      })}
+          </div>
+        </div>
+
+        <div className="checkoutWrapper">
+          <div className="summaryText">
+            <p>Summary</p>
+          </div>
+
+          <ul className="shippingOptions">
+            <li>
+              <DoneIcon /> Free home delivery for orders above 300 kr
+            </li>
+            <li>
+              <DoneIcon /> Pick up point near you available
+            </li>
+            <li>
+              <DoneIcon /> Free Click and Collect at our store near you
+            </li>
+          </ul>
+
+          <div className="subtotal">
+            <p>Subtotal</p>
+            <p>amout</p>
+          </div>
+
+          <div className="shippingProduct">
+            <p>Shipping Estimate</p>
+            <p>Free</p>
+          </div>
+
+          <div className="totalCheckout">
+            <p>
+            Total <small>(incl. taxes)</small>
+            </p>
+            <p>Total</p>
+          </div>
+
+          <button className="continueToCheckout">Continue to checkout</button>
+
+          <div className="secureCheckout">
+            <small>
+              <LockIcon /> secure checkout
+            </small>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }

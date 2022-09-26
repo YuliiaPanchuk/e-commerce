3dffaefbc8765310f9b947928e1b627d05af5994
Yuliia Panchuk <55352643+YuliiaPanchuk@users.noreply.github.com>
2022-09-26 21:49:08 +0200

    Create Icons for Section component

---
+++ src/components/HomeSection/HomeSection.tsx
@@ -0,0 +1,69 @@
+import { CardIcon } from './CardIcon';
+import { EasyReturnIcon } from './EasyReturnIcon';
+import { EnvironmentIcon } from './EnvironmentIcon';
+import { FreeDelivery } from './FreeDeliveryIcon';
+import { QualityIcon } from './QualityIcon';
+import { ShopOnline } from './ShopOnline';
+import "./HomeSection.css"
+
+export function HomeSection() {
+  return (
+    <>
+      <section className="sectionWrapper">
+        <div className="sectionContainer">
+          <QualityIcon />
+          <p className="sectionTitle">Best Quality</p>
+          <p className="sectionDescribtion">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua.
+          </p>
+        </div>
+
+        <div className="sectionContainer">
+          <CardIcon />
+          <p className="sectionTitle">Money secure</p>
+          <p className="sectionDescribtion">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua.
+          </p>
+        </div>
+
+        <div className="sectionContainer">
+          <EnvironmentIcon />
+          <p className="sectionTitle">Environment safe</p>
+          <p className="sectionDescribtion">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua.
+          </p>
+        </div>
+
+        <div className="sectionContainer">
+          <ShopOnline />
+          <p className="sectionTitle">Shop online</p>
+          <p className="sectionDescribtion">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua.
+          </p>
+        </div>
+
+        <div className="sectionContainer">
+          <FreeDelivery />
+          <p className="sectionTitle">Free delivery</p>
+          <p className="sectionDescribtion">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua.
+          </p>
+        </div>
+
+        <div className="sectionContainer">
+          <EasyReturnIcon />
+          <p className="sectionTitle">Easy return</p>
+          <p className="sectionDescribtion">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua.
+          </p>
+        </div>
+      </section>
+    </>
+  );
+}

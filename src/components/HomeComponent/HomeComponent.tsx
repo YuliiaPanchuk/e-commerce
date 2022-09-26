43fbc9757e3746fe6c4a95682f02d29b94ef7d4b
Yuliia Panchuk <55352643+YuliiaPanchuk@users.noreply.github.com>
2022-09-26 21:49:19 +0200

    Update Home component

--- src/components/HomeComponent/HomeComponent.tsx
+++ src/components/HomeComponent/HomeComponent.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
+import { HomeSection } from '../HomeSection/HomeSection';
 import './HomeComponent.css';
 
 export function HomeComponent() {
   return (
-    <div className='home'>
-      <div className="mainHomeImage">
-        <img src="/images/main.jpg" alt="" />
+    <>
+      <div className="home">
+        <div className="mainHomeImage">
+          <img src="/images/main.jpg" alt="" />
+        </div>
+
+        <div className="mainHeader">
+          <p>nom nom coffee</p>
+        </div>
       </div>
 
-      <div className="mainHeader">
-        <p>nom nom coffee</p>
-      </div>
-    </div>
+      <HomeSection />
+    </>
   );
 }

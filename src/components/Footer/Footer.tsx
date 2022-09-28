25ed58f223982bd36c11d197a92b6790c94e2c60
Yuliia Panchuk <55352643+YuliiaPanchuk@users.noreply.github.com>
2022-09-28 20:46:37 +0200

    Create Footer component

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import "./Footer.css"
+
+export function Footer() {
+  return (
+    <>
+      <section className="footer">
+        <div className="footerLabel">
+          <h1>This is label</h1>
+        </div>
+
+        <div className="footerInfo">
+          <p>Contact</p>
+          <small>We are here to help</small>
+          <small>nomnom@coffee.com</small>
+
+          <div className="footerWorkingHours">
+            <small>Monday to Friday</small>
+            <small>From 10am to 18.30pm</small>
+            <small>0046 760 123 123</small>
+          </div>
+        </div>
+
+        <div className="footerInfo">
+          <p>Help</p>
+          <small>Frequently Asked Questions</small>
+          <small>Stores</small>
+        </div>
+
+        <div className="footerInfo">
+          <p>Shop</p>
+          <small>Lorem lorem</small>
+          <small>Lorem lorem</small>
+          <small>Lorem lorem</small>
+        </div>
+
+      </section>
+    </>
+  )
+}

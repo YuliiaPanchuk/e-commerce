e7781202243b06564d36262ceb1f1e9753e6024a
Yuliia Panchuk <55352643+YuliiaPanchuk@users.noreply.github.com>
2022-09-30 06:20:08 +0200

    Crete News component

---
+++ src/components/News/News.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import './News.css';
+
+export function News() {
+  return (
+    <>
+      <section className="newsWrapper">
+        <div className="newsBlock">
+          <div className="imageNews">
+            <img src="/images/newsOne.jpg" alt="news" />
+          </div>
+
+          <div className="textNews">
+            <p className="textNewsTitle">Lorem ipsum dolor</p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+              incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+              exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            </p>
+          </div>
+        </div>
+
+        <div className="newsBlock">
+          <div className="textNews">
+            <p className="textNewsTitle">Lorem ipsum dolor</p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+              incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+              exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            </p>
+          </div>
+          
+          <div className="imageNews">
+            <img src="/images/newsTwo.jpg" alt="news" />
+          </div>
+        </div>
+
+        <div className="newsBlock">
+          <div className="imageNews">
+            <img src="/images/newsThree.jpg" alt="news" />
+          </div>
+
+          <div className="textNews">
+            <p className="textNewsTitle">Lorem ipsum dolor</p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+              incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+              exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            </p>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}

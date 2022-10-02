9c127c820702c70d1b27ef9fbe20f007dcde1eef
Yuliia Panchuk <55352643+YuliiaPanchuk@users.noreply.github.com>
2022-10-02 06:20:32 +0200

    Add component to Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,10 @@
 import { ContactForm } from "../components/Contact/ContactForm";
+import { Footer } from "../components/Footer/Footer";
 import { HomeComponent } from "../components/HomeComponent/HomeComponent";
 import { NavBar } from "../components/NavBar/NavBar";
+import { News } from "../components/News/News";
 import { UppBtn } from "../components/ScrollButton/UppBtn";
+import { WhyUs } from "../components/WhyUs.tsx/WhyUs";
 
 export function Home() {
   return (
@@ -9,6 +12,9 @@ export function Home() {
       <NavBar />
       <UppBtn />
       <HomeComponent />
+      <WhyUs />
+      <News />
+      <Footer />
     </>
   )
 }

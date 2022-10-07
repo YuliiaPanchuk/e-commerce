1693e72504d365c3f1c4a0bd335ab8ca71bc3053
Yuliia Panchuk <55352643+YuliiaPanchuk@users.noreply.github.com>
2022-10-07 06:17:48 +0200

    Update NavBarIcons

--- src/components/NavBarIcons/NavBarIcons.tsx
+++ src/components/NavBarIcons/NavBarIcons.tsx
@@ -1,7 +1,7 @@
-import React from "react"
+import React from 'react';
 import { ShoppingIcon } from './ShoppingIcon';
 import { UserProfile } from './UserProfile';
-import "./icons.css"
+import './icons.css';
 import { useShoppingCart } from '../../context/ShoppingCartContext';
 
 export function NavBarIcons() {
@@ -11,26 +11,26 @@ export function NavBarIcons() {
     <div className="iconsNavBarWrapper">
       <ul className="iconNavBarUl">
         <li>
-            <button
-              onClick={openCart}
-              style={{ width: '3rem', height: '2rem', position: 'relative' }}
-              className="navbarIconBtn"
+          <button
+            onClick={openCart}
+            style={{ width: '3rem', height: '2rem', position: 'relative' }}
+            className="navbarIconBtn"
+          >
+            <ShoppingIcon />
+            <div
+              style={{
+                color: 'black',
+                width: '1rem',
+                height: '1rem',
+                position: 'absolute',
+                bottom: 0,
+                right: 0,
+                transform: 'translate(-15%, -60%)',
+              }}
             >
-              <ShoppingIcon />
-              <div
-                style={{
-                  color: 'black',
-                  width: '1rem',
-                  height: '1rem',
-                  position: 'absolute',
-                  bottom: 0,
-                  right: 0,
-                  transform: 'translate(-15%, -60%)'
-                }}>
-                {cartQuantity}
-              </div>
-            </button>
-         
+              {cartQuantity}
+            </div>
+          </button>
         </li>
         <li>
           <button className="navbarIconBtn">

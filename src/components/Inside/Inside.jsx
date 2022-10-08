1b504854ca65df20a3903d526c879b109e1c04d3
Yuliia Panchuk <55352643+YuliiaPanchuk@users.noreply.github.com>
2022-10-08 20:42:58 +0200

    Create Inside component

---
+++ src/components/Inside/Inside.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+export function Inside() {
+  return (
+    <>
+      <section className="insideWrapper">
+        <h1>A look inside</h1>
+        <h1>to make a block of photos</h1>
+      </section>
+    </>
+  )
+}

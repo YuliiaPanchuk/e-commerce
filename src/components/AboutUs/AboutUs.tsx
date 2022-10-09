073d65d742e9165fe64b0a2aba7cb77e7f2a9207
Yuliia Panchuk <55352643+YuliiaPanchuk@users.noreply.github.com>
2022-10-09 08:47:28 +0200

    Css update

--- src/components/AboutUs/AboutUs.tsx
+++ src/components/AboutUs/AboutUs.tsx
@@ -3,15 +3,17 @@ import './AboutUs.css';
 export function AboutUs() {
   return (
     <section id="aboutUs">
+      <div className="aboutUsLeftContainer"></div>
+      <div className="aboutUsRightContainer"></div>
       <div className="aboutUsImages">
         <div className="aboutUsImageContainer">
-          <img src="images/NewsTwo.jpg" alt=""/>
+          <img src="images/NewsTwo.jpg" alt="" />
         </div>
         <div className="aboutUsImageContainer">
           <img src="images/Test.jpg" alt="" />
-        </div >
+        </div>
         <div className="aboutUsImageContainer">
-          <img src="images/Test.jpg" alt=""/>
+          <img src="images/Test.jpg" alt="" />
         </div>
         <div className="aboutUsImageContainer">
           <img src="images/NewsTwo.jpg" alt="" />
@@ -22,7 +24,9 @@ export function AboutUs() {
         <h1 className="aboutUsHeadline">NomNom Coffee Story</h1>
         <p className="aboutUsDescription">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-          ut labore et dolore magna aliqua. Ut enim ad minim veniam
+          ut labore et dolore magna aliqua.<br/> <br/> Ut enim ad minim veniam. Lorem ipsum dolor sit amet,
+          consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+          aliqua. Ut enim ad minim veniam.
         </p>
       </div>
     </section>

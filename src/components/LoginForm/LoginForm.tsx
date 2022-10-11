7a2fecb8da89696639cbffe69273d5c4bb0a831b
Yuliia Panchuk <55352643+YuliiaPanchuk@users.noreply.github.com>
2022-10-11 20:54:05 +0200

    Update LoginForm component

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,38 +1,44 @@
-import React, {useState} from "react"
-import { useNavigate } from "react-router-dom"
-import { signIn } from "../../api/account"
-import "./LoginForm.css"
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { signIn } from '../../api/account';
+import './LoginForm.css';
 
 export function LoginForm() {
-  const [name, setName] = useState("")
-  const [password, setPassword] = useState("")
+  const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
   const navigate = useNavigate();
 
   const handleSubmit = (e: any) => {
-    e.preventDefault()
+    e.preventDefault();
     signIn(name, password).then((data) => {
       if (data.success) {
-        navigate("/store")
+        navigate('/store');
+        alert('Welcome!');
+      } else {
+        alert(data.text);
       }
-      else {
-        alert(data.text)
-      }
-    })
-    
-  }
+    });
+  };
+
   return (
     <>
       <form onSubmit={handleSubmit}>
         <label htmlFor="">Username </label>
-        <input type="text" onChange={(e) => setName(e.target.value)}/>
-        
+        <input type="text" onChange={(e) => setName(e.target.value)} value={name} />
+
         <label htmlFor="">Password </label>
-        <input type="password" onChange={(e) => setPassword(e.target.value)}/>
-        
-        <button>
-          Log in
-        </button>
+        <input type="password" onChange={(e) => setPassword(e.target.value)} value={password} />
+    
+        <button>Log in</button>
       </form>
     </>
-  )
+  );
 }
+
+/*
+  TO DO:
+  3. save user log in data so it can be refreshed
+  2. add css to the input
+  4. display user icon and text on the top right corner
+  5. if logged in render the page and logout button.
+*/

bbdffa16d0ba7099dcf7591f05b4a7ae3e4b8d3e
Yuliia Panchuk <55352643+YuliiaPanchuk@users.noreply.github.com>
2022-10-15 20:26:23 +0200

    Add styling to Footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,37 +1,40 @@
-import React from "react";
-import "./Footer.css"
+import React from 'react';
+import { LogoFrame } from './LogoFrame';
+import './Footer.css';
 
 export function Footer() {
   return (
     <>
-      <section className="footer">
+      <section className='footerWrapper'>
+
+        <div className="footer">
         <div className="footerLabel">
-          <h1>This is label</h1>
+          <LogoFrame />
         </div>
+          <div className="footerInfo">
+            <p>Contact</p>
+            <small>We are here to help</small>
+            <small>nomnom@coffee.com</small>
 
-        <div className="footerInfo">
-          <p>Contact</p>
-          <small>We are here to help</small>
-          <small>nomnom@coffee.com</small>
-
-          <div className="footerWorkingHours">
-            <small>Monday to Friday</small>
-            <small>From 10am to 18.30pm</small>
-            <small>0046 760 123 123</small>
+            <div className="footerWorkingHours">
+              <small>Monday to Friday</small>
+              <small>From 10am to 18.30pm</small>
+              <small>0046 760 123 123</small>
+            </div>
           </div>
-        </div>
 
-        <div className="footerInfo">
-          <p>Help</p>
-          <small>Frequently Asked Questions</small>
-          <small>Stores</small>
-        </div>
+          <div className="footerInfo">
+            <p>Help</p>
+            <small>Frequently Asked Questions</small>
+            <small>Stores</small>
+          </div>
 
-        <div className="footerInfo">
-          <p>Shop</p>
-          <small>Lorem lorem</small>
-          <small>Lorem lorem</small>
-          <small>Lorem lorem</small>
+          <div className="footerInfo">
+            <p>Shop</p>
+            <small>Lorem lorem</small>
+            <small>Lorem lorem</small>
+            <small>Lorem lorem</small>
+          </div>
         </div>
       </section>
 
@@ -39,5 +42,5 @@ export function Footer() {
         <small>© 2022 Nom Nom Coffee | All Rights Reserved</small>
       </div>
     </>
-  )
+  );
 }

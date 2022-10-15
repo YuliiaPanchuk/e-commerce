74af640f5df5abb396cc9bd5798a8fc6800bcad4
Yuliia Panchuk <55352643+YuliiaPanchuk@users.noreply.github.com>
2022-10-15 20:31:09 +0200

    Add components

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,13 +1,17 @@
+import { Footer } from "../components/Footer/Footer";
 import { NavBar } from "../components/NavBar/NavBar";
 import { ProductCart } from "../components/ProductCart/ProductCart";
 import { UppBtn } from "../components/ScrollButton/UppBtn";
+import { SortButton } from "../components/SortButton/SortButton";
 
 export function Store() {
   return (
     <>
       <NavBar />
+      <SortButton />
       <ProductCart />
       <UppBtn />
+      <Footer/>
     </>
   )
 }

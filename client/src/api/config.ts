aa8e4ca6751219dceb920cdf88a0a52f0f9538e4
Yuliia Panchuk <55352643+YuliiaPanchuk@users.noreply.github.com>
2022-10-18 22:51:35 +0200

    Update config.ts

--- client/src/api/config.ts
+++ client/src/api/config.ts
@@ -1,2 +1,2 @@
 // export const API_ENDPOINT = 'http://localhost:3001';
-export const API_ENDPOINT = 'https://nom-nom-coffee-server.onrender.com:3001';
+export const API_ENDPOINT = 'https://nom-nom-coffee-server.onrender.com';
